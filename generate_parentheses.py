1a625a96d987628da9e53508a1a30cb58b2521ca
Parsa Yadollahi <parsayadollahi@gmail.com>
2019-10-13 21:27:12 -0400

    (medium) all valid permutations of parenthesis

---
+++ generate_parentheses.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+
+        ans = []
+        def gen_par(s = '', l = 0, r = 0):
+            if len(s) == 2*n:
+                ans.append(s)
+                return
+            # turns out the more you try to understand the solution
+            # the more you memorize it....
+            
+            if l < n:
+                gen_par(s+'(', l+1, r)
+            if r < l:
+                gen_par(s+')', l, r+1)
+
+        gen_par()
+        return ans
