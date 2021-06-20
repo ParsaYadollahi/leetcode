c279a1c73d248999d7c23ce6bcaa155d02c39c15
ParsaYadollahi <parsa.yadollahi@mail.mcgill.ca>
2021-06-20 14:40:14 -0400

    (M) rotate-image

--- rotate_matrix.py
+++ rotate_matrix.py
@@ -21,3 +21,31 @@ class Solution:
                 for k in range(4):
                     matrix[row][col] = temp_array[(k-1) % 4]
                     row, col = col, n-1-row
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        self.flip_matrix(matrix)
+        self.tilt_matrix(matrix)
+
+    def flip_matrix(self, matrix):
+        length = len(matrix) - 1
+
+        for i in range((length + 1) // 2):
+            for j in range((length + 1)):
+                matrix[i][j], matrix[length - i][j] = matrix[length - i][j], matrix[i][j]
+
+
+    def tilt_matrix(self, matrix):
+        length = len(matrix)
+        box, i = 0, 0
+        while(i <= length):
+            j = box
+            while(j < length):
+                matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+                j += 1
+            i += 1
+            box += 1
